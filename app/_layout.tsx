db5e13b1d65e4af98f1b2f0fec6a6487b5447a46
nuaip <mebarkinouaim@gmail.com>
2025-04-05 12:11:00 +0100

    wrapp the app with toastProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,38 @@
-import { Stack } from 'expo-router';
-import { CartProvider } from '@/context/CartContext';
-import { StyleSheet, View } from 'react-native';
-import CartIcon from '../components/CartIcon';
+import { Stack } from "expo-router";
+import { CartProvider } from "@/context/CartContext";
+import { StyleSheet, View } from "react-native";
+import CartIcon from "../components/CartIcon";
+import { ToastProvider } from "react-native-toast-notifications";
 
 export default function RootLayout() {
   return (
-    <CartProvider>
-      <View style={styles.container}>
-        <Stack
-          screenOptions={{
-            headerTitle: 'Fake Store',
-            headerRight: () => <CartIcon />,
-            headerStyle: styles.header,
-            headerTitleStyle: styles.headerTitle
-          }}
-        />
-      </View>
-    </CartProvider>
+    <ToastProvider>
+      <CartProvider>
+        <View style={styles.container}>
+          <Stack
+            screenOptions={{
+              headerTitle: "Fake Store",
+              headerRight: () => <CartIcon />,
+              headerStyle: styles.header,
+              headerTitleStyle: styles.headerTitle,
+            }}
+          />
+        </View>
+      </CartProvider>
+    </ToastProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5'
+    backgroundColor: "#f5f5f5",
   },
   header: {
-    backgroundColor: '#2f95dc'
+    backgroundColor: "#2f95dc",
   },
   headerTitle: {
-    color: 'white',
-    fontWeight: 'bold'
-  }
+    color: "white",
+    fontWeight: "bold",
+  },
 });
