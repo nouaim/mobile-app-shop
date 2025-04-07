b09550646a43962c2289a778307245df7ff1af27
nuaip <mebarkinouaim@gmail.com>
2025-04-07 18:41:49 +0100

    back route icon white

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout() {
           <Stack
             screenOptions={{
               headerTitle: "Fake Store",
+              headerTintColor: '#fff',
               headerRight: () => <CartIcon />,
               headerStyle: styles.header,
               headerTitleStyle: styles.headerTitle,
